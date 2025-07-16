a597556ec984d8071cfe7d1590fab32436f05a19
dharm4888 <dharm.oss4888@gmail.com>
2025-07-16 13:34:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def home():
     return render_template("index.html", prediction_text=prediction_text, confidence=confidence)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
